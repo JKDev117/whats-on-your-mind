b2eb73f2bf49b663e96f32b82b96e6b6bcd89f43
Jae Kim <jckim81@yahoo.com>
2019-12-22 23:45:14 -0500

    updated functions, added .one() method, etc.

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const searchObject = {
     query: null,
 };
 
+$('form').one('submit', function(event){
+  event.preventDefault();
+  $('label, select').removeClass('hidden');
+});
 
-$('form').submit(function(){
+
+$('form').submit(function(event){
     event.preventDefault();
-    if($('label').hasClass('hidden') || $('select').hasClass('hidden')){
-      $('label').removeClass('hidden');
-      $('select').removeClass('hidden');
-    }
     searchObject.query = $('#search-input').val();
     $('#news-sort-options').val("publishedAt");
     $('#video-sort-options').val("date");
@@ -40,28 +41,31 @@ $('#video-sort-options').change(function(){
 function fetchNewsQuery(searchterm){
     //to fetch content from NewsAPI
     fetch(`https://newsapi.org/v2/everything?q=${searchterm}&sortBy=${$('#news-sort-options').val()}&language=en&apiKey=2bf9f921521a4c8ea170fca30ab191ad`)
+        /*
         .then(response=>response.json())
         .then(responseJson=>api1response(responseJson))
-        .catch(error=>alert(`${error.message}`));                    
+        .catch(error=>alert(`${error.message}`));    
+        */
+        .then(response => {
+          if(response.ok)
+            return response.json();
+          return response.json().then(e => Promise.reject(e));
+        })      
+        .then(api1response)
+        .catch(error=>$('#newsAPI').text(`${error.message}`));                 
 };
 
-/*
-        .then(response => {
-          if(response.ok){
-            return response.json();
-          }
-          throw new Error(response.statusText);
-        })
-        .then(responseJson=>api1response(responseJson))
-        .catch(error=>$('#newsAPI').text(`${error.message}`));
-*/
 
 function fetchVideoQuery(searchterm){
     //to fetch content from YouTube Data API
     fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&order=${$('#video-sort-options').val()}&type=video&q=${searchterm}&key=AIzaSyChkVvlkXMt6Yc_F07bNPyZIyKUVUCVMTA`)
-        .then(res=>res.json())
-        .then(resJson=>api2response(resJson))
-        .catch(err=>alert('Something went wrong. Try again later.'));
+        .then(response => {
+          if(response.ok)
+            return response.json();
+          return response.json().then(e => Promise.reject(e));
+        })
+        .then(api2response)
+        .catch(error=>$('#youTubeAPI').text(`${error.message}`));  
 };
 
 
@@ -71,7 +75,7 @@ function api1response(responseJson){
     $('#newsAPI').append(
       responseJson.articles.map(article => `
         <h1>${article.title}</h1>
-        <p>${article.publishedAt}</p>  
+        <p>${article.publishedAt.slice(0,10)}</p>  
         <p>${article.description}</p>
         <a href="${article.url}" target=_blank>${article.url}</a>
         `)
@@ -83,7 +87,7 @@ function api2response(responseJson){
   console.log(responseJson);
   $('#youTubeAPI').append(
     responseJson.items.map(video => `
-        <p>${video.snippet.title} [${video.snippet.publishedAt}]</p>
+        <p>${video.snippet.title} [${video.snippet.publishedAt.slice(0,10)}]</p>
         <a href="https://youtu.be/${video.id.videoId}" target=_blank><img src="${video.snippet.thumbnails.default.url}" alt="video-search-image-thumbnail"/></a><br>
         <a href="https://youtu.be/${video.id.videoId}" target=_blank>https://youtu.be/${video.id.videoId}</a>    
         `)
